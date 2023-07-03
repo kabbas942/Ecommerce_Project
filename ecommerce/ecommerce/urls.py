9c2e28f5d8433e22059b3de2ea14408ecb7c19f0
Muhammad Abbas <91007241+kabbas942@users.noreply.github.com>
2023-07-03 22:17:54 +0500

    Fifth Commit

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ecommerce/',include("shoppingcart.urls")),
-    path('blog/',include("blog.urls"))
+    path('ecommerce/',include("shoppingcart.urls"))
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
