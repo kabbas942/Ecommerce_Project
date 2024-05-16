5b54d562b381ab06a865c0a2d8b72124cdb62db2
Muhammad Abbas <kabbas942@gmail.com>
2024-05-16 23:06:59 +0500

    commit-16May2024-2

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth import authenticate,login,logout,get_user_model
 from django.urls import reverse
+from django.contrib.auth.models import User
+from shoppingcart.models import Order,OrderDetail,Product
 # Create your views here.
 
 def signin(request):
@@ -36,35 +38,28 @@ def signout(request):
     logout(request)
     return redirect('/')
 
+def shippingAddress(request):
+        if request.method == 'POST':
+            userId = User.objects.get(id=request.user.id)
+            shippingAddress=request.POST.get("shippingAddress")
+            mobileNumber=request.POST.get("mobileNumber")
+            OrderNow= Order(customerId=userId,orderAddress=shippingAddress,orderMobileNumber=mobileNumber,orderPrice=33)
+            OrderNow.save()
+            for productDetailId,Qty in request.session['cart'].items(): 
+                product= Product.objects.get(productId=productDetailId)          
+                orderId = Order.objects.get(orderId = OrderNow.pk) 
+                OrderDetailNow = OrderDetail(productId=product, orderId=orderId, orderProductQuantity=Qty,productPrice=product.productPrice)
+                OrderDetailNow.save()                
+            request.session.flush()            
+            return render(request,"ecommerce/shippingAddress.html")
+
+
+
+
+
+
+
 '''
-#User Login
-def signIn(request):
-    if request.method=="POST":
-        userName = request.POST.get('userEmail')
-        userPassword = request.POST.get('userPassword')
-        authenticateUser = authenticate(username=userName, password=userPassword)
-        if authenticateUser is not None:
-            login(request,authenticateUser)
-            return redirect('/ecommerce')
-        else:
-            return render(request,'ecommerce/signIn.html')
-    return render(request,'ecommerce/signIn.html')
-
-#User Logout
-def logOutProfile(request):
-    logout(request)
-    return redirect('/ecommerce/signIn')
-
-#User signup
-def signUp(request):
-    if request.method=="POST":
-        userName= request.POST.get("userName")
-        userEmail= request.POST.get("userEmail")
-        userPassword= request.POST.get("userPassword")
-        getUserModel=get_user_model()
-        createUser = getUserModel.objects.create_user(username=userName, password=userPassword, email=userEmail)
-        createUser.save()
-    return render(request,"ecommerce/signIn.html")
 def shippingAddress(request):
     if request.user.is_anonymous:
         return redirect('/account/')   
@@ -73,9 +68,6 @@ def shippingAddress(request):
             userId = User.objects.get(id=request.user.id)
             shippingAddress=request.POST.get("shippingAddress")
             mobileNumber=request.POST.get("mobileNumber")
-            countryName=request.POST.get("countryName")
-            stateName=request.POST.get("stateName")
-            zipCode=request.POST.get("zipCode")
             OrderNow= Order(customerId=userId,orderAddress=shippingAddress,orderCountry=countryName,orderState=stateName,orderZipCode=zipCode,orderMobileNumber=mobileNumber,orderPrice=33)
             OrderNow.save()
             for productDetailId,Qty in request.session['cart'].items(): 
@@ -85,7 +77,6 @@ def shippingAddress(request):
                 OrderDetailNow.save()                
             request.session.flush()            
             return render(request,"ecommerce/shippingAddress.html")
-#Viewing Cart
 
 
 
